cc82aab4ce57de31f5c389db9d8917dddd64f4d9
RomaSamkov <Romansamkov.rs@gmail.com>
2022-07-13 22:17:36 +0300

    Update api.jsx

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,11 +1,10 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://pixabay.com/api/';
-const key = `26909086-a7a0f8caac98286b7fc7ad857`;
+const key = `27155173-9deaa55e537d9ae9b6e54e2b2`;
 const perPage = 12;
 
 function fetchImages(searchValue, currentPage) {
-  // console.log(searchValue);
   return axios
     .get(
       `?q=${searchValue}&page=${currentPage}&key=${key}&image_type=photo&orientation=horizontal&per_page=${perPage}`
