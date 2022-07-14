746bcdc908a7fad756132ad7cbd78f8b14f0ee73
RomaSamkov <Romansamkov.rs@gmail.com>
2022-07-14 12:03:34 +0300

    add src folders

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,71 +1,16 @@
-import { Component } from 'react';
-import Modal from 'components/Modal/Modal';
-import Searchbar from 'components/Searchbar/Searchbar';
-import ImageInfo from 'components/ImageInfo/ImageInfo';
-import Button from 'components/Button/Button';
-
-// import s from './App.module.css';
-
-class App extends Component {
-  state = {
-    showModal: false,
-    searchQuery: '',
-    page: 1,
-    src: '',
-    alt: '',
-    moreVisible: false,
-  };
-
-  // Функция для смены состояния модального окна с видимого на невидимое и получения данных для показа в модалке
-  toggleModal = e => {
-    this.setState(({ showModal }) => ({ showModal: !showModal }));
-
-    if (!this.state.showModal) {
-      this.setState({ src: e.target.dataset.src, alt: e.target.alt });
-    }
-  };
-
-  //Функция для получения из формы текста введенного пользователем в инпут
-  submitForm = e => {
-    this.setState({ page: 1 });
-    this.setState({ searchQuery: e.value });
-  };
-
-  // Функция для показа или скрытия кнопки "Загрузить еще"
-  moreButtonRender = () => {
-    this.setState({ moreVisible: true });
-  };
-  moreButtonHide = () => {
-    this.setState({ moreVisible: false });
-  };
-
-  clickMoreButton = e => {
-    this.setState(prevState => {
-      return { page: prevState.page + 1 };
-    });
-  };
-
-  render() {
-    const { showModal, moreVisible, searchQuery, page, src, alt } = this.state;
-    return (
-      <>
-        <Searchbar onSubmit={this.submitForm} />
-        <ImageInfo
-          searchQuery={searchQuery}
-          page={page}
-          onClick={this.toggleModal}
-          moreButtonRender={this.moreButtonRender}
-          moreButtonHide={this.moreButtonHide}
-        />
-        {moreVisible && <Button onClick={this.clickMoreButton} />}
-
-        {showModal && (
-          <Modal onClose={this.toggleModal}>
-            <img src={src} alt={alt} />
-          </Modal>
-        )}
-      </>
-    );
-  }
-}
-export default App;
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      React homework template
+    </div>
+  );
+};
